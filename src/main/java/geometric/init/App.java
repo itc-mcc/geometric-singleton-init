213ba8b10d90d621b976ba028c632c958117277b
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-11-06 15:33:32 -0700

    Deleting inital Code..

--- src/main/java/geometric/init/App.java
+++ src/main/java/geometric/init/App.java
@@ -4,11 +4,8 @@
 package geometric.init;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Factory App Assigment...");
     }
 }
